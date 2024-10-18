587e9844c2d3fc6c0557b87c89b1a139bda2abf2
Lucas Connors <lucas.revolutiontech@gmail.com>
2024-10-17 21:38:27 -0700

    observe: add LogRocket

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
+import LogRocket from "logrocket";
 
 import App from "./App";
+
+const LOG_ROCKET_APP_ID = "q6cfij/revolutiontechgithubio";
+
+// Init LogRocket
+if (process.env.NODE_ENV === "production") {
+  LogRocket.init(LOG_ROCKET_APP_ID);
+}
 
 const container = document.getElementById("app");
 const root = createRoot(container!);
