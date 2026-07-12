3447a4b80898560ad7507f054b08a7ee0c402bc9
Lucas Connors <lucas.revolutiontech@gmail.com>
2026-07-11 20:54:57 -0700

    copy: Directionally Correct -> North Star

--- src/project/SemanticSorting.tsx
+++ src/project/SemanticSorting.tsx
@@ -1,22 +1,22 @@
 import * as React from "react";
 
 import SEMANTIC_SORTING_RULES from "../assets/download/directionally-correct-rules.pdf";
-import SEMANTIC_SORTING_SELL_SHEET_IMAGE from "../assets/img/semantic-sorting/directionally-correct-sell-sheet.png";
-import SEMANTIC_SORTING_SELL_SHEET_PDF from "../assets/download/directionally-correct-sell-sheet.pdf";
+import SEMANTIC_SORTING_SELL_SHEET_IMAGE from "../assets/img/semantic-sorting/north-star-sell-sheet.png";
+import SEMANTIC_SORTING_SELL_SHEET_PDF from "../assets/download/north-star-sell-sheet.pdf";
 import { Platform, Project } from "./project";
 import { SellSheet } from "./SellSheet";
 
 export const PROJECT_SEMANTIC_SORTING: Project = {
-  name: "Directionally Correct",
+  name: "North Star",
   slug: "semantic-sorting",
   visible: false,
   year: 2026,
   description: (
     <>
-      Directionally Correct is a co-operative word guessing party game. One
-      player knows a secret word and must clue the other players by carefully
-      arranging their guesses, bringing them closer and closer to the correct
-      answer over the course of the game.
+      North Star is a co-operative word guessing party game. One player knows a
+      secret word and must clue the other players by carefully arranging their
+      guesses, bringing them closer and closer to the correct answer over the
+      course of the game.
     </>
   ),
   platforms: [Platform.BOARD_GAME],
